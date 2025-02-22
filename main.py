f56d530d326d7570c06bc761af4df53745504720
MUhammad Rafiq <rafiq85825@gmail.com>
2025-02-22 15:18:12 +0500

    Refactor API key handling and update endpoint method for generation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException, Header
-from dotenv import load_dotenv
-import os
 import ollama
+import os
+from dotenv import load_dotenv
 
-app = FastAPI()
 load_dotenv()
 
-API_KEY_CREDITS= {os.getenv("SECRET_KEY"), 5}
+API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
+print(API_KEY_CREDITS)
+app = FastAPI()
 
 def verify_api_key(x_api_key: str = Header(None)):
     credits = API_KEY_CREDITS.get(x_api_key, 0)
@@ -15,14 +16,10 @@ def verify_api_key(x_api_key: str = Header(None)):
 
     return x_api_key
 
-@app.get("/")
-def read_root():
-    return {"Hello World"}
-
-@app.get("/generate")
+@app.post("/generate")
 def generate(prompt: str, x_api_key: str = Depends(verify_api_key)):
     API_KEY_CREDITS[x_api_key] -= 1
-    response = ollama.chat(model="deepseek-r1", messages=[{"role": "user", "content": prompt}])
+    response = ollama.chat(model="mistral", messages=[{"role": "user", "content": prompt}])
     return {"response": response["message"]["content"]}
 
 if __name__ == "__main__":
